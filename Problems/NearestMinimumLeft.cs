e6f96d3dc117e88cdefe426902456033a585fb46
ijurao ijurao <ijurao@gmail.com>
2020-04-15 08:12:57 +1000

    15/04/2020

---
+++ Problems/NearestMinimumLeft.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmos.Problems
+{
+    class NearestMinimumLeft
+    {
+
+       
+      
+        public static int [] SequenceNearestMinimumLeft (int [] entry)
+        {
+            Stack<int> s = new Stack<int>();
+            
+            int[] result = new int[entry.Length];
+
+            for(int i = 0; i < entry.Length;i++)
+            {
+                var m = -1;
+                var n=0;
+                var lcambio = false;
+                while (s.TryPeek(out n) & entry[i] < n)
+                {
+                     m = s.Pop();
+                    lcambio = true;
+                }
+                if (s.Count == 0 &&  lcambio == false) result[i] = -1;
+                else result[i] = s.Peek();
+
+                s.Push(entry[i]);
+            }
+            return result;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Algoritmos.Problems;
+using System;
+
+namespace Algoritmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer problema numero menos inmediato a la izquierda:");
+            int[] arr = new int[] { 1, 6, 4, 10, 2, 5 };
+            printArr(arr);
+            int[] res = NearestMinimumLeft.SequenceNearestMinimumLeft(arr);
+            printArr(res);
+        }
+
+        static void printArr(int[] arr)
+        {
+            foreach(int i in arr)
+            {
+                Console.Write(i.ToString() + ",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
